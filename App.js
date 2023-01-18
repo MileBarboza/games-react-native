42c4a22d8556078248a11125aa70aaa4f7b6e92c
MileBarboza <barbozaamilena@hotmail.com>
2023-01-17 23:11:16 -0300

    optimizando la performance con flatlist

--- App.js
+++ App.js
@@ -1,21 +1,124 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react"
+import {Button, StyleSheet, View ,Text, FlatList, TouchableOpacity, Alert} from 'react-native';
+import Modal from "./src/components/Modal"
+import AddItem from "./src/components/AddItem"
+
 
 export default function App() {
+
+  const [ textItem, setTextItem ] = useState('') 
+  const [ list, setList ] = useState([]) 
+  const [ itemSelected, setItemSelected ] = useState("")  
+  const [ modalVisible, setModalVisible ] = useState(false)  
+
+
+  const onHandleChangeItem = text => {
+    setTextItem(text)
+  }
+
+  const addItem = () => {
+    if (textItem !== ""){
+        setList(prevState => [...prevState, textItem])
+        setTextItem("")
+      }else {
+      Alert.alert("Please type in something")
+    }
+  }
+  
+  const empty = () => {
+  return setList([])
+  }
+
+  const handleModal = item => {
+    setItemSelected(item)
+    setModalVisible(true)
+  }
+
+  const onHandleDelete = item => {
+    setList(prevState => prevState.filter(element => element !== item))
+    setModalVisible(!modalVisible)
+  }
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => handleModal(item)}  style={styles.renderItemStyle}>
+      <Text style={styles.txt}>{item}</Text>
+    </TouchableOpacity>
+  )
+
   return (
     <View style={styles.container}>
-      <Text>-.🎮.-</Text>
-      <Text>React Native</Text>
-      <StatusBar style="auto" />
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>My Task List</Text>
+
+        <AddItem
+          onChange={onHandleChangeItem}
+          textValue={textItem}
+          onAddItem={addItem}
+          onEmpty={empty} /> 
+      </View>
+
+      <View style={styles.listContainer}>
+        <FlatList
+          data={list}
+          renderItem={renderItem}
+          keyExtractor={item => item}
+        />
+      </View>
+
+      <Modal
+        isVisible={modalVisible}
+        itemSelected={itemSelected}
+        actionDeleteItem={() => onHandleDelete(itemSelected)}
+        onDismissModal={setModalVisible}
+      />
+         <Button title='EMPTY' onPress={empty} color='#863737' style={styles.btn}/> 
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E7EAF2",
+    backgroundColor: "#222",
   },
-});
+  titleContainer: {
+    height: 200,
+    paddingHorizontal: 30,
+    paddingTop: 80,
+  },
+  title: {
+    textAlign:'center',
+    marginBottom: 30,
+    fontSize: 40,
+    fontWeight: "500",
+    color: "#fff",
+  },
+  listContainer: {
+    flex: 2,
+    marginHorizontal: 30,
+    marginTop: 40,
+    padding: 3,
+  },
+  renderItemStyle: {
+    height: 70,
+    flexDirection: "row",
+    marginBottom: 15,
+    backgroundColor: "white",
+    backgroundColor: "#444",
+    borderRadius: 10,
+    justifyContent:'space-between',
+    justifyContent:'center',
+    alignItems: "center",
+    shadowColor: "black",
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 1 },
+    shadowRadius: 2,
+    elevation: 3,
+  },
+  txt: {
+    color: "#fff",
+    fontSize:20,
+    fontWeight: 'bold',
+  }
+})
